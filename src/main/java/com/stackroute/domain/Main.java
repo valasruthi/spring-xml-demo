9246df957d36140e3171226de96facf66141265c
valasruthi <valasruthi4@mail.com>
2019-07-29 06:27:25 +0530

    bean lifecycle

--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -1,23 +1,18 @@
 package com.stackroute.domain;
 
-import com.stackroute.domain.Actor;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+
 
 
 public class Main {
 public static void main(String[] args){
-  XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("beans.xml"));
-  Movie movie1= (Movie)xmlBeanFactory.getBean("movie");
-  ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-  Movie movie=(Movie) context.getBean("movie");
-  System.out.println(movie.getActor());
- movie.setApplicationContext(context);
- movie.setBeanFactory(xmlBeanFactory);
- movie.setBeanName("beandata");
+  ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+
+  ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
+  BeanLifeCycle beanLifeCycle = (BeanLifeCycle) applicationContext.getBean("cycle");
+
 
 }
 }
